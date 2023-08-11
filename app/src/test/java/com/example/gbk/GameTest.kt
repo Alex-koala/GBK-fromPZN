53b3e71776044368b010fac1e134f01e89a0a4b2
Alex-koala <alexas.aa123@gmail.com>
2023-08-11 19:54:18 +0700

    test logic engine

--- app/src/test/java/com/example/gbk/GameTest.kt
+++ app/src/test/java/com/example/gbk/GameTest.kt
@@ -20,4 +20,27 @@ class GameTest {
         Assert.assertEquals(R.drawable.scissors, Game.pickDrawable("SCISSORS"))
 
     }
+
+
+    @Test
+    fun testIsDraw() {
+        Assert.assertTrue(Game.isDraw("ROCK", "ROCK"))
+        Assert.assertTrue(Game.isDraw("PAPER", "PAPER"))
+        Assert.assertTrue(Game.isDraw("SCISSORS", "SCISSORS"))
+
+        Assert.assertFalse(Game.isDraw("ROCK", "PAPER"))
+        Assert.assertFalse(Game.isDraw("PAPER", "SCISSORS"))
+        Assert.assertFalse(Game.isDraw("SCISSORS", "ROCK"))
+    }
+
+    @Test
+    fun testIsWin() {
+        Assert.assertTrue(Game.isWin("ROCK", "SCISSORS"))
+        Assert.assertTrue(Game.isWin("SCISSORS", "PAPER"))
+        Assert.assertTrue(Game.isWin("PAPER", "ROCK"))
+
+        Assert.assertFalse(Game.isWin("ROCK", "PAPER"))
+        Assert.assertFalse(Game.isWin("SCISSORS", "ROCK"))
+        Assert.assertFalse(Game.isWin("PAPER", "SCISSORS"))
+    }
 }
